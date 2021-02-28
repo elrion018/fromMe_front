7cc805c09131cbaecc956498ff8c5110040146ac
Daniel <elrion018@gmail.com>
2021-02-28 23:32:09 +0900

    feat : add HomeNavigator into AppNaviator

--- src/navigators/AppNavigator/index.tsx
+++ src/navigators/AppNavigator/index.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+
+import {HomeNavigator} from '../../navigators';
 import {LoginPage, RegisterPage} from '../../pages/mainPages';
 
 export enum AppNavigatorPagesAndStacks {
   Login = 'Login',
   Register = 'Register',
+  HomeStack = 'HomeStack',
 }
 
 export type AppNavigatorParamList = {
   Login: undefined;
   Register: undefined;
+  HomeStack: undefined;
 };
 
 const AppNavigatorStack = createStackNavigator<AppNavigatorParamList>();
 
 const AppNavigator: React.FunctionComponent = () => {
   return (
-    <AppNavigatorStack.Navigator initialRouteName={'Register'}>
+    <AppNavigatorStack.Navigator
+      initialRouteName={AppNavigatorPagesAndStacks.HomeStack}>
       <AppNavigatorStack.Screen
         name={AppNavigatorPagesAndStacks.Login}
         component={LoginPage}
@@ -24,6 +29,10 @@ const AppNavigator: React.FunctionComponent = () => {
       <AppNavigatorStack.Screen
         name={AppNavigatorPagesAndStacks.Register}
         component={RegisterPage}
+      />
+      <AppNavigatorStack.Screen
+        name={AppNavigatorPagesAndStacks.HomeStack}
+        component={HomeNavigator}
       />
     </AppNavigatorStack.Navigator>
   );
--- src/navigators/HomeNavigator/index.tsx
+++ src/navigators/HomeNavigator/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import {HomePage} from '../../pages/homePages';
 
-const HomeNavigatorStack = createStackNavigator();
+export enum HomeNavigatorPagesAndStacks {
+  Home = 'Home',
+}
+
+export type HomeNavigatorParamList = {
+  Home: undefined;
+};
+
+const HomeNavigatorStack = createStackNavigator<HomeNavigatorParamList>();
 
 const HomeNavigator: React.FunctionComponent = () => {
-  return <HomeNavigatorStack.Navigator></HomeNavigatorStack.Navigator>;
+  return (
+    <HomeNavigatorStack.Navigator
+      initialRouteName={HomeNavigatorPagesAndStacks.Home}>
+      <HomeNavigatorStack.Screen
+        name={HomeNavigatorPagesAndStacks.Home}
+        component={HomePage}
+      />
+    </HomeNavigatorStack.Navigator>
+  );
 };
 
 export default HomeNavigator;
